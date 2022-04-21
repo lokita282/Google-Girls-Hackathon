38802f9b6bb026dd979ee903839b68bdb8fe2c93
Priyambi P Hiran <79246912+priyambi@users.noreply.github.com>
2022-04-21 19:47:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import TabNavigation from './src/screens/TabNavigation'
+
+const App = () => {
+  return (
+    <TabNavigation/>
+  )
+}
+
+export default App;
